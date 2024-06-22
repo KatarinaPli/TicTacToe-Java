17a5a8bec355637aaf0c3b589e7845d1af2000a2
Kateryna Shyrokova <kateryna.shyrokova@powercoders.org>
2024-06-22 15:12:14 +0200

    Added a constructor for boardPannel

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -5,17 +5,37 @@ import javax.swing.*;
 
 
 public class TicTacToe {
-    int boardWidth = 600;
-    int boardHeight = 650; //50px for the text panel on top
 
-    JFrame board = new JFrame();
+    int textWidth = 600;
+    int textHeight = 650; //50px for the text panel on top
+
+    JFrame frame = new JFrame("Tic-Tac-Toe");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
 
     public TicTacToe(){
-        board.setVisible(true);
-        board.setSize(boardWidth, boardHeight);
-        board.setLocationRelativeTo(null); //set the frame in the center of the screen
-        board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        board.setResizable(false);
-        board.setLayout(new BorderLayout());
+        frame.setVisible(true);
+        frame.setSize(textWidth, textHeight);
+        frame.setLocationRelativeTo(null); //set the frame in the center of the screen
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setBackground(Color.LIGHT_GRAY);
+        textPanel.setForeground(Color.magenta);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 40));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Tic Tac Toe");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(3,3));
+        boardPanel.setBackground(Color.orange);
+        frame.add(boardPanel);
+        
     }
 }
